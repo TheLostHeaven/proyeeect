9f90c08cbf0465a8dd65bad14e79bb75640762e4
Dani <dannyking07@outlook.es>
2025-07-07 20:20:04 -0500

    Remove unused assets and update service worker configurations
    - Deleted unused image (Logo.png), font (MaterialIcons-Regular.otf), and icon font (CupertinoIcons.ttf) files from flutter assets.
    - Removed no_sleep.js script from wakelock_web package as it is no longer needed.
    - Deleted the ink_sparkle.frag shader file from flutter assets.
    - Updated flutter_bootstrap.js to use the latest service worker version.
    - Updated flutter_service_worker.js with the new hash for flutter_bootstrap.js.
    - Simplified vercel.json by removing unnecessary outputDirectory configuration.

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5845a87094976b433eaccd111f4dc9f1
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f0cf6bb9c2bab62a1cbaef6e4cdb1642",
+"flutter_bootstrap.js": "4e9a614e6a375dc54260d3a8857b1ef1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
